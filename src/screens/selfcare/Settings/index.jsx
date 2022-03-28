ee99634069ea8b43b21e2419723b5c6d0da78a63
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-28 02:59:20 +0200

    use logout by redux

--- src/screens/selfcare/Settings/index.jsx
+++ src/screens/selfcare/Settings/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { useDispatch } from 'react-redux'
+
+import { logout } from '@actions/auth'
+
+import { Button } from '@components/styled-components'
 
 const Index = () => {
+    const dispatch = useDispatch()
+
     return (
         <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <Text>Hello Banana Cloud</Text>
             <Text>Settings</Text>
+            <Button title="Deconnexion" icon="log-out-outline" onPress={() => dispatch(logout())} />
         </View>
     )
 }
