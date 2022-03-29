cc173b16decdcaa2fe1d4d87a339ab4e538ba116
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-30 01:21:52 +0200

    fix splash screen android/ios

--- android/app/src/main/java/com/bananacloud/MainActivity.java
+++ android/app/src/main/java/com/bananacloud/MainActivity.java
@@ -5,6 +5,11 @@ import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -13,11 +18,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "bananacloud";
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
   }
 }
