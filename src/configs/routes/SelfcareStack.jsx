ac47abc0c71e465a9f1be3a587cbc5abc1997d40
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-29 17:49:56 +0200

    update tab nav

--- src/configs/routes/SelfcareStack.jsx
+++ src/configs/routes/SelfcareStack.jsx
@@ -1,35 +1,61 @@
 import React from 'react'
+import { Text, Button } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import IonIcons from 'react-native-vector-icons/Ionicons'
 IonIcons.loadFont()
 
-import Home from '@screens/selfcare/Home'
-import Settings from '@screens/selfcare/Settings'
+import Photos from '@screens/selfcare/Photos'
+import Search from '@screens/selfcare/Search'
+import Library from '@screens/selfcare/Library'
 
 const Tab = createBottomTabNavigator()
 
+const HeaderTitle = () => (
+    <Text style={{ fontWeight: 'bold', fontSize: 21 }}>
+        🍌 Banana<Text style={{ color: '#f39c12' }}>Cloud</Text>
+    </Text>
+)
+
 const SelfcareStack = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ navigation }) => ({
+                headerTitle: props => <HeaderTitle {...props} />,
+                headerRight: () => (
+                    <Button
+                        onPress={() => navigation.navigate('Profile')}
+                        color="#000"
+                        title="aaa"
+                    />
+                ),
+            })}>
             <Tab.Screen
-                name="Home"
-                component={Home}
+                name="Photos"
+                component={Photos}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <IonIcons name="home-outline" color={color} size={size} />
+                        <IonIcons name="images-outline" color={color} size={size} />
                     ),
-                    headerTitle: '🍌 Banana Cloud',
-                    tabBarLabel: 'Accueil',
                 }}
             />
             <Tab.Screen
-                name="Settings"
-                component={Settings}
+                name="Search"
+                component={Search}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <IonIcons name="settings-outline" color={color} size={size} />
+                        <IonIcons name="search-outline" color={color} size={size} />
                     ),
-                    title: 'Réglages',
+                    title: 'Rechercher',
+                }}
+            />
+            <Tab.Screen
+                name="Library"
+                component={Library}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <IonIcons name="library-outline" color={color} size={size} />
+                    ),
+                    title: 'Bibliothèque',
                 }}
             />
         </Tab.Navigator>
