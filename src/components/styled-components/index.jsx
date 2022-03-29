3a3a7d4140da8315a46410c17a23931b52ab112b
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-29 17:48:47 +0200

    add GoBack styled-components

--- src/components/styled-components/index.jsx
+++ src/components/styled-components/index.jsx
@@ -1,5 +1,6 @@
 import { Container } from './Container'
 import { Button } from './Button'
 import { TextInput } from './TextInput'
+import { GoBack } from './GoBack'
 
-export { Container, Button, TextInput }
+export { Container, Button, TextInput, GoBack }
