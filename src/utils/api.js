5965378d3f3b1ad600743ce617bef827bc592a18
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-30 12:51:26 +0200

    detect if we are in prod or dev mode

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
-import { API_BASE } from '@env'
+import { API_BASE, API_BASE_DEV, NODE_ENV } from '@env'
+
+const dev = NODE_ENV === 'dev'
 
 const api = axios.create({
-    baseURL: API_BASE,
+    baseURL: dev ? API_BASE_DEV : API_BASE,
 })
 
 export default api
