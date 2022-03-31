82de98ccd66a5a6e1ab3a6a4c32254b5338f73c4
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-31 15:50:18 +0200

    update login & register style

--- src/screens/guest/Login/index.jsx
+++ src/screens/guest/Login/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { View, TouchableOpacity, Text, Image } from 'react-native'
+import { SafeAreaView, View, TouchableOpacity, Text, Image } from 'react-native'
 import { useDispatch } from 'react-redux'
 
 import { login } from '@actions/auth'
 
-import { Container, TextInput, Button } from '@components/styled-components'
+import { Padding, TextInput, Button } from '@components/styled-components'
 
 import logo from '@root/assets/bananacloud.png'
 
@@ -17,50 +17,59 @@ const Index = ({ navigation }) => {
     })
 
     return (
-        <Container>
-            <View
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                <Image
-                    source={logo}
-                    style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 20 }}
-                />
-                <Text style={{ fontWeight: 'bold', fontSize: 32, marginBottom: 20 }}>
-                    Se connecter
-                </Text>
-                <TextInput
-                    placeholder="john.doe@bananacloud.com"
-                    onChangeText={e => setUser({ ...user, email: e })}
-                    color="#dfe6e9"
-                    icon="person-outline"
-                />
-                <TextInput
-                    placeholder="********"
-                    onChangeText={e => setUser({ ...user, password: e })}
-                    secureTextEntry
-                    color="#dfe6e9"
-                    icon="key-outline"
-                />
-                <Button
-                    title="Valider"
-                    onPress={() => dispatch(login(user))}
-                    style={{ bgColor: '#00b894' }}
-                />
-                <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+        <SafeAreaView style={{ backgroundColor: '#00dafe', height: '100%' }}>
+            <Padding pl={10} pr={10}>
+                <View
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        backgroundColor: '#00dafe',
+                    }}>
+                    <Image
+                        source={logo}
+                        style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 20 }}
+                    />
                     <Text
                         style={{
-                            textDecorationLine: 'underline',
-                            textAlign: 'center',
-                            marginTop: 20,
+                            fontWeight: 'bold',
+                            fontSize: 32,
+                            marginBottom: 20,
+                            color: '#ffffff',
                         }}>
-                        Je créé mon compte
+                        Se connecter
                     </Text>
-                </TouchableOpacity>
-            </View>
-        </Container>
+                    <TextInput
+                        placeholder="john.doe@bananacloud.com"
+                        onChangeText={e => setUser({ ...user, email: e })}
+                        color="#dfe6e9"
+                        icon="person-outline"
+                    />
+                    <TextInput
+                        placeholder="********"
+                        onChangeText={e => setUser({ ...user, password: e })}
+                        secureTextEntry
+                        color="#dfe6e9"
+                        icon="key-outline"
+                    />
+                    <Button
+                        title="Valider"
+                        onPress={() => dispatch(login(user))}
+                        style={{ bgColor: '#00b894' }}
+                    />
+                    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                        <Text
+                            style={{
+                                textDecorationLine: 'underline',
+                                textAlign: 'center',
+                                marginTop: 20,
+                                color: '#ffffff',
+                            }}>
+                            Je créé mon compte
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Padding>
+        </SafeAreaView>
     )
 }
 
--- src/screens/guest/Register/index.jsx
+++ src/screens/guest/Register/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { SafeAreaView, View, Text, TouchableOpacity, Image } from 'react-native'
 
 import { register } from '@actions/auth'
 
-import { Container, TextInput, Button } from '@components/styled-components'
+import { Padding, TextInput, Button } from '@components/styled-components'
 
 import logo from '@root/assets/bananacloud.png'
 
@@ -18,57 +18,66 @@ const Index = ({ navigation }) => {
     })
 
     return (
-        <Container>
-            <View
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                <Image
-                    source={logo}
-                    style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 20 }}
-                />
-                <Text style={{ fontWeight: 'bold', fontSize: 32, marginBottom: 20 }}>
-                    S'enregistrer
-                </Text>
-                <TextInput
-                    placeholder="john.doe@bananacloud.com"
-                    onChangeText={e => setUser({ ...user, email: e })}
-                    color="#dfe6e9"
-                    icon="person-outline"
-                />
-                <TextInput
-                    placeholder="********"
-                    onChangeText={e => setUser({ ...user, password: e })}
-                    secureTextEntry
-                    color="#dfe6e9"
-                    icon="key-outline"
-                />
-                <TextInput
-                    placeholder="********"
-                    onChangeText={e => setUser({ ...user, confirmPassword: e })}
-                    secureTextEntry
-                    color="#dfe6e9"
-                    icon="key-outline"
-                />
-                <Button
-                    title="Valider"
-                    onPress={() => dispatch(register(user))}
-                    style={{ bgColor: '#00b894' }}
-                />
-                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <SafeAreaView style={{ backgroundColor: '#00dafe', height: '100%' }}>
+            <Padding pl={10} pr={10}>
+                <View
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    <Image
+                        source={logo}
+                        style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 20 }}
+                    />
                     <Text
                         style={{
-                            textDecorationLine: 'underline',
-                            textAlign: 'center',
-                            marginTop: 20,
+                            fontWeight: 'bold',
+                            fontSize: 32,
+                            marginBottom: 20,
+                            color: '#ffffff',
                         }}>
-                        J'ai déjà un compte
+                        S'enregistrer
                     </Text>
-                </TouchableOpacity>
-            </View>
-        </Container>
+                    <TextInput
+                        placeholder="john.doe@bananacloud.com"
+                        onChangeText={e => setUser({ ...user, email: e })}
+                        color="#dfe6e9"
+                        icon="person-outline"
+                    />
+                    <TextInput
+                        placeholder="********"
+                        onChangeText={e => setUser({ ...user, password: e })}
+                        secureTextEntry
+                        color="#dfe6e9"
+                        icon="key-outline"
+                    />
+                    <TextInput
+                        placeholder="********"
+                        onChangeText={e => setUser({ ...user, confirmPassword: e })}
+                        secureTextEntry
+                        color="#dfe6e9"
+                        icon="key-outline"
+                    />
+                    <Button
+                        title="Valider"
+                        onPress={() => dispatch(register(user))}
+                        style={{ bgColor: '#00b894' }}
+                    />
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text
+                            style={{
+                                textDecorationLine: 'underline',
+                                textAlign: 'center',
+                                marginTop: 20,
+                                color: '#ffffff',
+                            }}>
+                            J'ai déjà un compte
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Padding>
+        </SafeAreaView>
     )
 }
 
