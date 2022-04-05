9498ced0d5a3a3b78df09fd0bf679a63405cc03b
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-06 01:47:27 +0200

    refactoring

--- App.jsx
+++ App.jsx
@@ -11,23 +11,15 @@ import Routes from '@configs/routes'
 import { darkTheme, lightTheme } from '@configs/themes'
 import { persistor, store } from '@configs/store'
 
-import './src/configs/translations/initTranslation'
+import '@configs/translations/initTranslation'
 
 admob()
     .setRequestConfiguration({
-        // Update all future requests suitable for parental guidance
         maxAdContentRating: MaxAdContentRating.PG,
-
-        // Indicates that you want your content treated as child-directed for purposes of COPPA.
         tagForChildDirectedTreatment: true,
-
-        // Indicates that you want the ad request to be handled in a
-        // manner suitable for users under the age of consent.
         tagForUnderAgeOfConsent: true,
     })
-    .then(() => {
-        console.log('Configuration done !')
-    })
+    .then()
 
 export default function App() {
     const isDarkMode = useColorScheme() === 'dark'
