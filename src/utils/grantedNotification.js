abf52a3ca4f092136a553e1c99168653d535d4d8
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-07 02:53:27 +0200

    utilities

--- src/utils/grantedNotification.js
+++ src/utils/grantedNotification.js
@@ -1,9 +1,5 @@
 import messaging from '@react-native-firebase/messaging'
 
 export const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission()
-
-    if (authStatus) {
-        console.log('Authorization status:', authStatus)
-    }
+    await messaging().requestPermission()
 }
