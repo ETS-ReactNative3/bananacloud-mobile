7e0a49675f916acbc6141db0712f8e32ecb86767
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 20:14:48 +0200

    update text component

--- src/components/styled-components/StyledText.jsx
+++ src/components/styled-components/StyledText.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const StyledText = ({ children, color, bold, title }) => (
-    <TextView color={color} bold={bold} title={title}>
+export const StyledText = ({ children, color, bold, title, center }) => (
+    <TextView color={color} bold={bold} title={title} center={center}>
         {children}
     </TextView>
 )
@@ -11,4 +11,5 @@ const TextView = styled.Text`
     color: ${({ color, theme }) => (color ? color : theme.colors.secondary)};
     font-weight: ${props => (props.bold ? 'bold' : 'normal')};
     font-size: ${props => (props.title ? '21px' : '16px')};
+    text-align: ${props => (props.center ? 'center' : 'left')};
 `
