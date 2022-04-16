8a03d4b3ef9ebde050d1af2407391607275e133d
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 15:08:07 +0200

    display pub if not premium, else doesn't

--- src/screens/selfcare/Search/index.jsx
+++ src/screens/selfcare/Search/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 import { InterstitialAd, TestIds, AdEventType } from '@react-native-firebase/admob'
 
+import { StyledText, Margin, Button } from '@components/styled-components'
+
 const Search = () => {
+    const { t } = useTranslation()
+
+    const isPremium = useSelector(state => state.user.isPremium)
+
     const showInterstitialAd = () => {
+        if (isPremium) {
+            return alert(t('search.alreadyPremium'))
+        }
+
         const interstitialAd = InterstitialAd.createForAdRequest(TestIds.INTERSTITIAL)
 
         interstitialAd.onAdEvent((type, error) => {
@@ -21,17 +32,16 @@ const Search = () => {
 
     return (
         <MainView>
-            <TouchableOpacity onPress={showInterstitialAd}>
-                <Text>Show pub</Text>
-            </TouchableOpacity>
+            <Margin mt={10} />
+            <Button onPress={showInterstitialAd} title={t('search.showPubBtn')} />
         </MainView>
     )
 }
 
 const MainView = styled.View`
-    display: 'flex';
-    justify-content: 'center';
-    align-items: 'center';
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export default Search
