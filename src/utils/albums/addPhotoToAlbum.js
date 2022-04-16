2e0de19506d44beba767ccb560e5e1730eecf58b
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 19:44:15 +0200

    await download file :D

--- src/utils/albums/addPhotoToAlbum.js
+++ src/utils/albums/addPhotoToAlbum.js
@@ -14,41 +14,41 @@ export const addPhotoToAlbum = async (albumName, photoPath, userId) => {
         RNFS.downloadFile({
             fromUrl: albumContentPath,
             toFile: `${RNFS.DocumentDirectoryPath}/${albumName}_old.json`,
-        })
+        }).promise.then(async () => {
+            content = JSON.parse(
+                await RNFS.readFile(`${RNFS.DocumentDirectoryPath}/${albumName}_old.json`),
+            )
 
-        content = JSON.parse(
-            await RNFS.readFile(`${RNFS.DocumentDirectoryPath}/${albumName}_old.json`),
-        )
+            let photoExist = content.findIndex(el => {
+                if (el.path === photoPath) {
+                    return el.path
+                }
+            })
 
-        let photoExist = content.findIndex(el => {
-            if (el.path === photoPath) {
-                return el.path
+            if (photoExist === -1) {
+                content = [...content, { path: photoPath }]
+                const albumPath = `${RNFS.DocumentDirectoryPath}/${albumName}.json`
+
+                await RNFS.writeFile(albumPath, JSON.stringify(content), 'utf8')
+                await storage().ref(`/${userId}/albums/${albumName}.json`).putFile(albumPath)
+
+                await RNFS.unlink(albumPath)
+
+                showMessage({
+                    message: 'Ajouté avec succès',
+                    type: 'info',
+                })
+            } else {
+                showMessage({
+                    message: 'Photo déjà dans album',
+                    type: 'info',
+                })
             }
         })
-
-        if (photoExist === -1) {
-            content = [...content, { path: photoPath }]
-            const albumPath = `${RNFS.DocumentDirectoryPath}/${albumName}.json`
-
-            await RNFS.writeFile(albumPath, JSON.stringify(content), 'utf8')
-            await storage().ref(`/${userId}/albums/${albumName}.json`).putFile(albumPath)
-
-            await RNFS.unlink(albumPath)
-
-            showMessage({
-                message: 'Ajouté avec succès',
-                type: 'info',
-            })
-        } else {
-            showMessage({
-                message: 'Photo déjà dans album',
-                type: 'info',
-            })
-        }
     } catch (error) {
         console.log(error)
         showMessage({
-            message: "Une erreur c'est produite",
+            message: `Une erreur : ${error}`,
             type: 'warning',
         })
     }
