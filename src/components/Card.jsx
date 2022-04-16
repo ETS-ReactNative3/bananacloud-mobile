fbd1e45354f5fb9023f7f7ba428efc7b67f6ed99
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 15:04:57 +0200

    refacto/translate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {
     View,
     Image,
@@ -9,29 +9,21 @@ import {
     FlatList,
 } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import IonIcons from 'react-native-vector-icons/Ionicons'
 
-import { getAlbums } from '@utils/albums/getAlbums'
 import { addPhotoToAlbum } from '@utils/albums/addPhotoToAlbum'
 
-import { Button, Margin } from './styled-components'
+import { Button, Margin, Container } from './styled-components'
 
-const Card = ({ photo, isAlbum }) => {
+const Card = ({ photo }) => {
+    const { t } = useTranslation()
     const navigation = useNavigation()
     const userId = useSelector(state => state.user.user._id)
-    const favoritesList = true
+    const albumsList = useSelector(state => state.album.albumsList)
 
-    const [albumsList, setAlbumsList] = useState([])
-
-    const hydrateAlbums = async () => {
-        const list = await getAlbums(userId)
-        setAlbumsList(list)
-    }
-
-    useEffect(() => {
-        hydrateAlbums()
-    }, [])
+    const favoritesList = false
 
     const [isLoading, setIsLoading] = useState(true)
     const [visible, setIsVisible] = useState(false)
@@ -66,51 +58,50 @@ const Card = ({ photo, isAlbum }) => {
                         padding: 5,
                     }}
                 >
-                    {isAlbum === 'true' ? (
-                        <></>
-                    ) : (
-                        <>
-                            <TouchableOpacity onPress={() => console.log(photo)}>
-                                {favoritesList ? (
-                                    <IonIcons name="heart" color="#f39c12" size={21} />
-                                ) : (
-                                    <IonIcons name="heart-outline" size={21} />
-                                )}
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => setIsVisible(true)}>
-                                <IonIcons name="folder" color="#f39c12" size={21} />
-                            </TouchableOpacity>
-                        </>
-                    )}
+                    <>
+                        <TouchableOpacity onPress={() => console.log(photo)}>
+                            {favoritesList ? (
+                                <IonIcons name="heart" color="#f39c12" size={21} />
+                            ) : (
+                                <IonIcons name="heart-outline" color="#f39c12" size={21} />
+                            )}
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setIsVisible(true)}>
+                            <IonIcons name="folder" color="#f39c12" size={21} />
+                        </TouchableOpacity>
+                    </>
                 </View>
             </View>
 
             {isLoading && (
+                // Exception style 🥲
                 <ActivityIndicator
                     size={32}
                     style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
                 />
             )}
 
-            <Modal visible={visible}>
-                <Text>Placer l'image dans un album</Text>
+            <Modal visible={visible} onPress={() => setIsVisible(false)}>
+                <Container>
+                    <Text>{t('card.placePicture')}</Text>
 
-                <FlatList
-                    showsVerticalScrollIndicator={false}
-                    legacyImplementation={false}
-                    data={albumsList}
-                    renderItem={({ item }) => (
-                        <Margin mb={5} mt={5} ml={5} mr={5}>
-                            <Button
-                                title={item}
-                                onPress={() => handleAddPhotoToAlbum(item)}
-                                icon="folder"
-                                style={{ color: '#000000', bgColor: '#ffffff' }}
-                            ></Button>
-                        </Margin>
-                    )}
-                    keyExtractor={item => item}
-                />
+                    <FlatList
+                        showsVerticalScrollIndicator={false}
+                        legacyImplementation={false}
+                        data={albumsList}
+                        renderItem={({ item }) => (
+                            <Margin mb={5} mt={5} ml={5} mr={5}>
+                                <Button
+                                    title={item}
+                                    onPress={() => handleAddPhotoToAlbum(item)}
+                                    icon="folder"
+                                    style={{ color: '#000000', bgColor: '#ffffff' }}
+                                ></Button>
+                            </Margin>
+                        )}
+                        keyExtractor={item => item}
+                    />
+                </Container>
             </Modal>
         </TouchableOpacity>
     )
