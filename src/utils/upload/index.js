ba6f9465712131dfab8267e969d4ef9893f79d1e
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 18:12:32 +0200

    replace to media reducer

--- src/utils/upload/index.js
+++ src/utils/upload/index.js
@@ -1,31 +1,4 @@
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
-import storage from '@react-native-firebase/storage'
-import { showMessage } from 'react-native-flash-message'
-
-export const uploadImage = async (photo, userId) => {
-    const uri = photo
-    const filename = uri.substring(uri.lastIndexOf('/') + 1)
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-
-    const task = storage().ref(`${userId}/${filename}`).putFile(uploadUri)
-    // set progress state
-    // task.on('state_changed', snapshot => {
-    //     setTransferred(Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000)
-    // })
-    try {
-        await task
-        showMessage({
-            message: `Success`,
-            type: 'success',
-        })
-    } catch (e) {
-        console.error(e)
-        showMessage({
-            message: `${e}`,
-            type: 'warning',
-        })
-    }
-}
 
 export const uploadFromGallery = async () => {
     const result = await launchImageLibrary({
