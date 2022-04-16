baf64fae5dfad25ee62cc9bfca903791a669694d
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-04-16 18:11:40 +0200

    add media to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux'
 import user from './user'
 import theme from './theme'
 import langage from './langage'
+import media from './media'
 import album from './album'
 
-export default combineReducers({ user, theme, langage, album })
+export default combineReducers({ user, theme, langage, album, media })
